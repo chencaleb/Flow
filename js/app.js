4763ba810dd512a6768bbfe167990f39cfe3185f
chencaleb <clb_chen@berkeley.edu>
2016-02-03 14:31:56 -0800

    semi-works

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ var requestAnimationFrame = window.requestAnimationFrame ||
 
 window.onload = function() {
 
+var circles = new Array();
+
 //finds midpoint between two points
 function findMidpoint(p1, p2) {
   return {
@@ -16,11 +18,72 @@ function findMidpoint(p1, p2) {
   };
 }
 
+//circle constructor
+function Circle(radius, speed, width, xPos, yPos) {
+  this.radius = radius;
+  this.speed = speed;
+  this.width = width;
+  this.xPos = xPos;
+  this.yPos = yPos;
+
+  this.counter = 0;
+
+  var signHelper = Math.random();
+
+  if(signHelper > 0.5) {
+    this.sign = -1;
+  } else {
+    this.sign = 1;
+  }
+}
+
+//circle object methods
+Circle.prototype.update = function() {
+  this.counter += this.sign * this.speed;
+  ctx.beginPath();
+  ctx.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
+          this.yPos + Math.sin(this.counter / 100) * this.radius,
+          this.width,
+          0,
+          Math.PI * 2,
+          false);
+  ctx.closePath();
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fill();
+};
+
+
+//makes the circles
+function createCircles() {
+  for (var i=0; i<200; i++) {
+    var randomX = Math.round(-200 + Math.random() * 1000);
+    var randomY = Math.round(-200 + Math.random() * 800);
+    var speed = 0.2 + Math.random() * 3;
+    var size = 5;
+
+    var circle = new Circle(100, speed, size, randomX, randomY);
+    circles.push(circle);
+  }
+  draw();
+}
+createCircles();
+
+
+function draw() {
+  ctx.clearRect(0, 0, 800, 600);
+  for(var i=0; i<circles.length; i++) {
+    var myCircle = circles[i];
+    myCircle.update();
+  }
+  requestAnimationFrame(draw);
+}
+
+
 //line properties
 ctx.lineWidth = 7;
 ctx.lineJoin = ctx.lineCap = 'round';
 ctx.shadowBlur = 7;
-ctx.shadowColor = 'rgb(0, 0, 0)';
+ctx.shadowColor = 'rgb(255, 255, 255)';
 
 var isDrawing, points = [ ];
 
@@ -64,64 +127,6 @@ myCanvas.onmouseup = function() {
   isDrawing = false;
   points.length = 0;
 };
-
-var circles = new Array();
-
-function Circle(radius, speed, width, xPos, yPos) {
-  this.radius = radius;
-  this.speed = speed;
-  this.width = width;
-  this.xPos = xPos;
-  this.yPos = yPos;
-
-  this.counter = 0;
-
-  var signHelper = Math.floor(Math.random() * 2);
-
-  if(signHelper ==1) {
-    this.sign = -1;
-  } else {
-    this.sign = 1;
-  }
-}
-
-Circle.prototype.update = function() {
-  this.counter += this.sign * this.speed;
-  ctx.beginPath();
-  ctx.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
-          this.yPos + Math.sin(this.counter / 100) * this.radius,
-          this.width,
-          0,
-          Math.PI * 2,
-          false);
-  ctx.closePath();
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fill();
-};
-
-function createCircles() {
-  for (var i=0; i<200; i++) {
-    var randomX = Math.round(-200 + Math.random() * 1000);
-    var randomY = Math.round(-200 + Math.random() * 800);
-    var speed = 0.2 + Math.random() * 3;
-    var size = 5;
-
-    var circle = new Circle(100, speed, size, randomX, randomY);
-    circles.push(circle);
-  }
-  draw();
-}
-createCircles();
-
-
-function draw() {
-  ctx.clearRect(0, 0, 800, 600);
-  for(var i=0; i<circles.length; i++) {
-    var myCircle = circles[i];
-    myCircle.update();
-  }
-  requestAnimationFrame(draw);
-}
 
 // function fadeOut() {
 //   context.fillStyle = "rgba(255,255,255,0.1)";
