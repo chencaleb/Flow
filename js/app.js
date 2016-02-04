152578cfce2cf1298e89d76fd104cc4aa81938e0
chencaleb <clb_chen@berkeley.edu>
2016-02-03 23:27:09 -0800

    starting from scratch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
-var canvas = document.getElementById('myCanvas');
-var bounds = canvas.getBoundingClientRect(); //get reference to bounds
-var ctx = canvas.getContext("2d"); //get reference to the drawing context
-var requestAnimationFrame = window.requestAnimationFrame || 
-                            window.mozRequestAnimationFrame ||
-                            window.webkitRequestAnimationFrame ||
-                            window.msRequestAnimationFrame;
+var canvas;
+var ctx;
+var bounds;
+var requestAnimationFrame;
+var mouseXPos;
+var mouseYPos;
+var isMouseDown;
 
 window.onload = function() {
+requestAnimationFrame = window.requestAnimationFrame || 
+                        window.mozRequestAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.msRequestAnimationFrame;
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d"); //get reference to the drawing context
+bounds = canvas.getBoundingClientRect(); //get reference to bounds
+
+
+//creates mouse input listeners
+document.addEventListener("mousemove", mouseMoveFunction, false);
+document.addEventListener("mousedown", mouseDownFunction, false);
+document.addEventListener("mouseup", mouseUpFunction, false);
+
+player = new Player();
+};
+
+function mouseMoveFunction(event) {
+  mouseXPos = event.clientX - bounds.left;
+  mouseYPos = event.clientY - bounds.top;
+}
+
+function mouseDownFunction(event) {
+  isMouseDown = true;
+}
+
+function mouseUpFunction(event) {
+  isMouseDown = false;
+}
 
 var circles = new Array();
 
@@ -68,72 +98,3 @@ function draw() {
   }
   requestAnimationFrame(draw);
 }
-
-
-//line properties
-ctx.lineWidth = 7;
-ctx.lineJoin = ctx.lineCap = 'round';
-ctx.shadowBlur = 7;
-ctx.shadowColor = 'rgb(255, 255, 255)';
-
-var isDrawing, points = [ ];
-
-//finds midpoint between two points
-function findMidpoint(p1, p2) {
-  return {
-    x: p1.x + (p2.x - p1.x) / 2,
-    y: p1.y + (p2.y - p1.y) / 2
-  };
-}
-
-//mouse events
-myCanvas.onmousedown = function(e) {
-  isDrawing = true;
-  points.push({ x: e.clientX-bounds.left, y: e.clientY-bounds.top });
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-};
-
-myCanvas.onmousemove = function(e) {
-  if (!isDrawing) return;
-  
-  points.push({ x: e.clientX-bounds.left, y: e.clientY-bounds.top });
-
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  
-  var p1 = points[0];
-  var p2 = points[1];
-  
-  ctx.beginPath();
-  ctx.moveTo(p1.x, p1.y);
-  console.log(points);
-
-  for (var i = 1, len = points.length; i < len; i++) {
-    // we pick the point between pi+1 & pi+2 as the
-    // end point and p1 as our control point
-    var midPoint = findMidpoint(p1, p2);
-    ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
-    p1 = points[i];
-    p2 = points[i+1];
-  }
-  // Draw last line as a straight line while
-  // we wait for the next point to be able to calculate
-  // the bezier control point
-  ctx.lineTo(p1.x, p1.y);
-  ctx.stroke();
-};
-
-myCanvas.onmouseup = function() {
-  isDrawing = false;
-  points.length = 0;
-};
-
-// function fadeOut() {
-//   context.fillStyle = "rgba(255,255,255,0.1)";
-//   context.fillRect(0, 0, canvas.width, canvas.height);
-//   setTimeout(fadeOut, 500);
-// }
-
-// canvas.addEventListener('mousemove', onMouseEvent, false);
-// fadeOut();
-};
