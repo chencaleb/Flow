1f7ed0f9ae5816156fc578e6ecd1f37e33323bc6
chencaleb <clb_chen@berkeley.edu>
2016-02-05 03:05:13 -0800

    cleaned up working code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
+var requestAnimationFrame;
 var canvas;
 var ctx;
-var bounds;
-var requestAnimationFrame;
 
+var bounds;
 var mouseXPos;
 var mouseYPos;
 
@@ -10,10 +10,9 @@ var startButton;
 var isMouseDown = false;
 var hasStarted = false;
 
-
+var player;
 var enemies = [];
 var circles = [];
-var player;
 
 var speed = {x: -2, y: 1.5};
 var time = 0;
@@ -33,7 +32,6 @@ timer = document.getElementById("timer");
 document.addEventListener("mousemove", mouseMoveFunction, false);
 document.addEventListener("mousedown", mouseDownFunction, false);
 document.addEventListener("mouseup", mouseUpFunction, false);
-
 startButton.addEventListener("click", startButtonFunction, false);
 
 player = new Player();
@@ -49,12 +47,11 @@ function startButtonFunction(event) {
     var aud = document.getElementById("song");
     aud.load();
     aud.play();
-    aud.volume = 0.5; 
+    aud.volume = 0.5;
+    aud.loop = true; 
 
     rules.style.display = "none";
     gg.innerHTML = "";
-
-
     enemies = [];
     player.userPiece = [];
     
@@ -84,6 +81,33 @@ function gameOver() {
   rules.style.display="block";
   gg.innerHTML = "Game Over!!";
 }
+//Keeps track of current positions
+function Coordinates(x, y) {
+  this.position = {x: x, y: y};
+}
+
+//calculate distance between points
+Coordinates.prototype.distanceTo = function(circle) {
+  var distX = circle.x-this.position.x;
+  var distY = circle.y=this.position.y;
+  return Math.sqrt(distX^2 + distY^2);
+};
+
+//player attributes
+function Player() {
+  this.position = {x: 0, y: 0};
+  this.size = 8;
+  this.userPiece = [];
+}
+Player.prototype = new Coordinates();
+
+//enemy attributes
+function Enemy() {
+  this.position = {x: 0, y: 0};
+  this.size = 6 + (Math.random() * 5);
+  this.movement = 1 + (Math.random() * 0.5);
+}
+Enemy.prototype = new Coordinates();
 
 function evaluator() {
   
@@ -190,138 +214,35 @@ function randomizeCircle(circle) {
   return circle;
 }
 
-//Keeps track of current positions
-function Coordinates(x, y) {
-  this.position = {x: x, y: y};
-}
+/**
+  *   PSEUDO CODE
+  *
+  **/
 
-//calculate distance between points
-Coordinates.prototype.distanceTo = function(circle) {
-  var distX = circle.x-this.position.x;
-  var distY = circle.y=this.position.y;
-  return Math.sqrt(distX^2 + distY^2);
-};
+/**
+Build event listeners/constructors
 
-//player attributes
-function Player() {
-  this.position = {x: 0, y: 0};
-  this.size = 8;
-  this.userPiece = [];
-}
-Player.prototype = new Coordinates();
+if game has started (mousedown/mousemove)
+  keep track of mouse position
 
-//enemy attributes
-function Enemy() {
-  this.position = {x: 0, y: 0};
-  this.size = 6 + (Math.random() * 5);
-  this.movement = 1 + (Math.random() * 0.5);
-}
-Enemy.prototype = new Coordinates();
+Animation
+
+instead of draw and clear to animate, establish a path using mouse position
+and append position into an array that creates a line of set length
+    create new array to store position of mouse
+    for loop to iterate through position array
+    if length of array gets too high, "shift" to pop off first index
+    this will create a short line segment without having to animate
+    functions beginPath, stroke, closePath to initiate sequence of drawing line
+    use arc to provide for shape, fill to color the line, and sin/cos will give it a flowing movement
 
 
+if mouse goes off canvas, game over
+    coordinates for canvas are 0,0
+    if mouse has negative coordinate == game over
+    if mouse exceeds right and bottom of canvas
 
+generate circles and animate circles
 
-//if game has started (mousedown/mousemove) --check boolean
-//keep track of mouse position
-//generate circles and animate circles
-
-//draw mouse movement using sin/cos curves (quadraticCurveTo)
-//instead of draw and clear to animate, establish a path using mouse position
-//and append position into an array that creates a line of set length
-    //create new array to store position of mouse
-    //for loop to iterate through position array
-    //if length of array gets too high, "shift" to pop off first index
-    //this will create a short line segment without having to animate
-    //functions beginPath, stroke, closePath to initiate sequence of drawing line
-    //use arc, fill to color the line and give it a flowing movement
-
-
-//if mouse goes off canvas, game over
-    //coordinates for canvas are 0,0
-    //if mouse has negative coordinate == game over
-
-
-
-
-
-//if mouse position == enemy position, game over
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //circle constructor
-// function Circle(radius, speed, width, xPos, yPos) {
-//   this.radius = radius;
-//   this.speed = speed;
-//   this.width = width;
-//   this.xPos = xPos;
-//   this.yPos = yPos;
-
-//   this.counter = 0;
-
-//   var signHelper = Math.random();
-
-//   if(signHelper > 0.5) {
-//     this.sign = -1;
-//   } else {
-//     this.sign = 1;
-//   }
-// }
-
-// //circle object methods
-// Circle.prototype.update = function() {
-//   this.counter += this.sign * this.speed;
-//   ctx.beginPath();
-//   ctx.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
-//           this.yPos + Math.sin(this.counter / 100) * this.radius,
-//           this.width,
-//           0,
-//           Math.PI * 2,
-//           false);
-//   ctx.closePath();
-//   ctx.fillStyle = 'rgb(0, 0, 0)';
-//   ctx.fill();
-// };
-
-
-//makes the circles
-// function createCircles() {
-//   for (var i=0; i<200; i++) {
-//     var randomX = Math.round(-200 + Math.random() * 1000);
-//     var randomY = Math.round(-200 + Math.random() * 800);
-//     var speed = 0.2 + Math.random() * 3;
-//     var size = 5;
-
-//     var circle = new Circle(100, speed, size, randomX, randomY);
-//     circles.push(circle);
-//   }
-//   draw();
-// }
-// createCircles();
-
-// function draw() {
-//   ctx.clearRect(0, 0, 800, 600); //overwrites pointer
-//   for(var i=0; i<circles.length; i++) {
-//     var myCircle = circles[i];
-//     myCircle.update();
-//   }
-//   requestAnimationFrame(draw);
-// }
+if mouse position == enemy position, game over
+**/
