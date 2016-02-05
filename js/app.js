330306a97d1f45e42da2ad46ec3957219edacd28
chencaleb <clb_chen@berkeley.edu>
2016-02-05 02:38:11 -0800

    created timer and game over message

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ var circles = [];
 var player;
 
 var speed = {x: -2, y: 1.5};
-
+var time = 0;
 
 window.onload = function() {
 requestAnimationFrame = window.requestAnimationFrame || 
@@ -28,7 +28,7 @@ canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d"); //get reference to the drawing context
 bounds = canvas.getBoundingClientRect(); //get reference to bounds
 startButton = document.getElementById("startButton");
-
+timer = document.getElementById("timer");
 //creates mouse input listeners
 document.addEventListener("mousemove", mouseMoveFunction, false);
 document.addEventListener("mousedown", mouseDownFunction, false);
@@ -51,12 +51,17 @@ function startButtonFunction(event) {
     aud.play();
     aud.volume = 0.5; 
 
+    rules.style.display = "none";
+    gg.innerHTML = "";
+
 
     enemies = [];
     player.userPiece = [];
     
     player.position.x = mouseXPos;
     player.position.y = mouseYPos;
+
+    time = new Date().getTime();
   }
 }
 
@@ -76,7 +81,8 @@ function mouseUpFunction(event) {
 
 function gameOver() {
   hasStarted = false;
-  console.log("Game Over");
+  rules.style.display="block";
+  gg.innerHTML = "Game Over!!";
 }
 
 function evaluator() {
@@ -138,8 +144,7 @@ function evaluator() {
     //Checks for collisions
     // if(hasStarted) {
     //   if(circle.distanceTo(player.position) < (player.size + circle.size)/2) {
-    //     gameOver();
-
+    //      gameOver();
     //   }
     // }
 
@@ -163,6 +168,11 @@ function evaluator() {
   //Continues to generate enemies as they leave canvas
   if(enemies.length < 40) {
     enemies.push(randomizeCircle(new Enemy()));
+  }
+
+  if(hasStarted) {
+    counter = "Time: <span>" + Math.floor(((new Date().getTime() - time) / 1000 ) * 100) / 100 + "</span>";
+    timer.innerHTML = counter;
   }
 }
 
